ba6e61661d265379c818cb261364cdc7d8c660be
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-25 22:57:21 +0200

    add styles ofr header button

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
 
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 
-import './common.scss';
+const App = () => {
+	const weekStartDate = new Date();
+	const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
-class App extends Component {
-	state = {
-		weekStartDate: new Date(),
-	};
-
-	render() {
-		const { weekStartDate } = this.state;
-		const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
-
-		return (
-			<>
-				<Header />
-				<Calendar weekDates={weekDates} />
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<Header />
+			<Calendar weekDates={weekDates} />
+		</>
+	);
+};
 
 export default App;
