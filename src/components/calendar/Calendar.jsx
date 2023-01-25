cb4c902003c2756c923ede850e4846fe81c920c8
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-25 20:31:09 +0200

    push

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -4,29 +4,31 @@ import Navigation from './../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
 import events from '../../gateway/events';
-
 import './calendar.scss';
 
 class Calendar extends Component {
-  state = {
-    events,
-  };
+	state = {
+		events,
+	};
 
-  render() {
-    const { weekDates } = this.props;
+	render() {
+		const { weekDates } = this.props;
 
-    return (
-      <section className="calendar">
-        <Navigation weekDates={weekDates} />
-        <div className="calendar__body">
-          <div className="calendar__week-container">
-            <Sidebar />
-            <Week weekDates={weekDates} events={this.state.events} />
-          </div>
-        </div>
-      </section>
-    );
-  }
+		return (
+			<section className="calendar">
+				<Navigation weekDates={weekDates} />
+				<div className="calendar__body">
+					<div className="calendar__week-container">
+						<Sidebar />
+						<Week
+							weekDates={weekDates}
+							events={this.state.events}
+						/>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Calendar;
