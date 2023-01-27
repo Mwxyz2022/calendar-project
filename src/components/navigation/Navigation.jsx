89e2320774a0e5ddc2a79a5eb2ddec243717ffac
Mwxyz2022 <rozhukvlad@gmail.com>
2023-01-27 12:35:49 +0200

    add current day marker

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 
 import { days } from '../../utils/dateUtils.js';
 
+const TODAY = new Date().setHours(0, 0, 0, 0);
+
 const Navigation = ({ weekDates }) => {
+	console.log(TODAY);
 	return (
 		<header className="calendar__header">
-			{weekDates.map((dayDate) => (
-				<div
-					className="calendar__day-label day-label"
-					key={dayDate.getDay()}
-				>
-					<span className="day-label__day-name">
-						{days[dayDate.getDay()]}
-					</span>
-					<span className="day-label__day-number">
-						{dayDate.getDate()}
-					</span>
-				</div>
-			))}
-			<div className="current_day"></div>
+			{weekDates.map((dayDate) => {
+				console.log(dayDate.getTime());
+				const styleDay =
+					TODAY !== dayDate.getTime()
+						? `day-label__day-number`
+						: `day-label__day-number today `;
+
+				return (
+					<div
+						className="calendar__day-label day-label  "
+						key={dayDate.getDay()}
+					>
+						<span className="day-label__day-name">
+							{days[dayDate.getDay()]}
+						</span>
+
+						<span className={styleDay}>{dayDate.getDate()}</span>
+					</div>
+				);
+			})}
 		</header>
 	);
 };
